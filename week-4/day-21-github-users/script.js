9fbe28d65916a643385082632a6273a553e691f5
Nikki Everett <nikki.everett@outlook.com>
2017-04-11 23:03:45 -0500

    github repo assignment

--- week-4/day-21-github-users/script.js
+++ week-4/day-21-github-users/script.js
@@ -1,30 +1,33 @@
-var promise = $.ajax({
-  url: 'https://api.github.com/search/users?q=aquaman'
-});
-
+var searchQuery = document.querySelector('#query');
 var userList = document.querySelector('#user-list');
 
-promise.done(function(data){
-  console.log(data);
 
-  for(var i = 0; i < data.items.length; i ++){
-    var li = document.createElement('li');
-    userList.appendChild(li);
-
-    var avatar = document.createElement('img');
-    avatar.src = data.items[i].avatar_url;
-    userList.appendChild(avatar);
-
-    var user =document.createElement('a');
-    user.textContent =  data.items[i].login;
-    user.href = data.items[i].html_url;
-    userList.appendChild(user);
-    console.log(user);
-
-    var score = document.createElement('p');
-    score.textContent = "rating: " + data.items[i].score;
-    userList.appendChild(score);
-
+query.addEventListener('keyup', function(evt){
+  if(evt.keyCode !== 13){
+    return;
   }
+  userList.innerHTML = '';
+  var promise = $.ajax({
+    url: ('https://api.github.com/search/repositories?q=' + searchQuery.value)
+  });
+  promise.done(function(data) {
+    console.log(data);
+    for(var i = 0; i < data.items.length; i ++){
+      var li = document.createElement('li');
+      li.classList.add('entry');
+      userList.appendChild(li);
 
+      var repoName = document.createElement('a');
+      repoName.classList.add('repo');
+      repoName.textContent = data.items[i].name;
+      repoName.href = data.items[i].html_url;
+      li.appendChild(repoName);
+
+      var user = document.createElement('a');
+      user.classList.add('user');
+      user.textContent = 'by ' + data.items[i].owner.login;
+      user.href = data.items[i].owner.html_url;
+      li.appendChild(user);
+    }
+  });
 });
