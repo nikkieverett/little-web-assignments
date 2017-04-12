346121c2ef442ddfe05ec5ede9b49cebf50e3f9a
Nikki Everett <nikki.everett@outlook.com>
2017-04-11 21:52:08 -0500

    colege scorecard assignment

---
+++ week-4/day-22-scorecard/script.js
@@ -0,0 +1,14 @@
+var fs = require('fs');
+
+var fileData = fs.readFile('Most-Recent-Cohorts-Scorecard-Elements.csv', 'utf-8', function(err, data){
+  var lines = data.split('\r\n');
+
+  for(var i = 1; i < lines.length-1; i++){
+    var line = lines[i];
+    var pieces = line.split(',');
+    if(pieces[32] !== 'NULL'){
+      console.log('Name: ' + pieces[3]);
+      console.log('ACT Score: ' + pieces[32]);
+    }
+  }
+});
